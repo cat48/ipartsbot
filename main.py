bdfa6913a4ad9cbdce3759af674acc7128ec6a5b
dev <cat48@nate.com>
2017-07-25 16:41:50 +0900

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+__metaclass__ = type
+
+from microsoftbotframework import MsBot
+from tasks import *
+import os
+
+# bot = MsBot(port=int(os.environ['PORT']))
+bot = MsBot()
+bot.add_process(echo_response)
+bot.run()
---
+++ tasks.py
@@ -0,0 +1,5 @@
+from microsoftbotframework import ReplyToActivity
+
+def echo_response(message):
+    if message["type"] == "message":
+        ReplyToActivity(fill=message, text=message["text"]).send()
