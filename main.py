076b9f008ca3285cc125e54d83364fd7ef1cd791
dev <cat48@nate.com>
2017-07-26 09:49:35 +0900

    commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from microsoftbotframework import MsBot
 from tasks import *
 import os
 
-# bot = MsBot(port=int(os.environ['PORT']))
 bot = MsBot()
 bot.add_process(echo_response)
-bot.run()
+
+if __name__ == '__main__':
+    bot.run()
